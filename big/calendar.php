8ec58ce58e7708eab9b7d391548352ba9cdb6d65
derekdesigns <82245525+derekdesigns@users.noreply.github.com>
2021-05-31 12:48:04 -0700

    Add files via upload

---
+++ big/calendar.php
@@ -0,0 +1,32 @@
+<?php include 'includes/header.php';?>
+
+<h1>Calendar</h1>
+
+<!--Show/Hide Month View Calendar based on resolution-->
+<div class="month-view embed-container">
+  <iframe src="https://calendar.google.com/calendar/embed?src=v15td70ifkj908ro57a21k4fps%40group.calendar.google.com&ctz=America%2FLos_Angeles">
+  </iframe>
+</div>
+
+<!--Show/Hide Agenda View Calendar based on resolution-->
+<div class="agenda-view embed-container">
+  <iframe src='https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&src=djE1dGQ3MGlma2o5MDhybzU3YTIxazRmcHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%237CB342&color=%237986CB&mode=AGENDA' style='border:solid 1px #777' width='800' height='600' frameborder='0' scrolling='no'>
+  </iframe>
+</div>
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+  <h2><u>Upcoming Events</u></h2>
+  <h3>Built to Spill</h3>
+  <h4>Doors @ 7pm<br />
+      Show @ 8pm<br />
+      $29.50</h4>
+
+  
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php';?>
