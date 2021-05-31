c0e4b66a853d77ea308236f8184d8931011b085e
derekdesigns <82245525+derekdesigns@users.noreply.github.com>
2021-05-31 13:54:52 -0700

    Add files via upload

---
+++ it162/aia.php
@@ -0,0 +1,93 @@
+<?php include 'includes/header.php';?>
+
+<section>
+<h1>Client Info</h1>
+
+<table class="client">
+  <tr>
+    <td><h4>Client's Name</h4></td>
+    <td>Wendy Clark (Final decision maker)</td>
+  </tr>
+  <tr>
+    <td><h4>Client's Website Name</h4></td>
+    <td>To Be Determined (Wendy has a logo with "Eek!", which refers to one of her art projects years ago.  It is professionally made and holds sentimental value, but is not descriptive of what she is currently producing).</td>
+  </tr>
+  <tr>
+    <td><h4>Redesign or Brand New Website</h4></td>
+    <td>Brand Spankin' New</td>
+  </tr>
+  <tr>
+    <td><h4>Type of Website</h4></td>
+    <td>Marketing/Shopping</td>
+  </tr>
+  <tr>
+    <td><h4>Client's Goals</h4></td>
+    <td>To showcase her art.</td>
+  </tr>
+</table>
+</section>
+
+<section>
+<h1>Audience Info</h1>
+
+<table class="audience">
+    <tr>
+        <th>Audience</th>
+        <th>Issues, Concerns, and Needs</th>
+        <th>Approach</th>
+    </tr>
+    <tr>
+        <td>
+          <h4>Facebook Friends</h4>
+          <p>Aside from old fashioned word-of-mouth, Facebook is Wendy's only current method of advertising her paintings. She has a large and diverse network of friends, a slight majority being women aged 40-55.  They are professionals, often in the cruise industry. All are tech savvy, with most Facebook exchanges taking place on iphones (safari browser).</p></td>
+        <td>
+          <ul>
+            <li>Facebook is so casual and informal, many friends may be scrolling just long enough to enjoy the pic without ever realising it is a painting for sale.</li>
+            <li>After just a couple days a post may seem outdated, even if an item is still very much available.</li>
+            <li>Wendy's 500 "friends" are limiting the scope of potential sales.</li>
+          </ul>
+        </td>
+        <td>
+          <ul>
+            <li>A bonafide website will clearly indicate what is currently for sale.</li>
+            <li>If demand proves to be there, a shopping cart can be implemented down the road.</li>
+            <li>Search engine optimization would dwarf Wendy's current reachabilty.</li>
+          </ul> 
+        </td>
+    </tr>
+    <tr>
+        <td><h4>Pet Owners</h4>
+        <p>Most of the money Wendy makes is through commissioned work, and the niche she has found is producing pet portraits. Buyers are a bit more likely to be dog owners, although Wendy has immortalized many a kitten as well. This group cannot be typecast by age or income. They would be more likely to get off their phones to research her past works and onto their PCs using Chrome.</p></td>
+        <td>
+          <ul>
+            <li>Want a professionally done website to peruse before committing to a significant purchase.</li>
+            <li>Comfort-level is increased with the ability to view an extensive catalogue of past works.</li>
+          </ul>
+        </td>
+        <td>
+          <ul>
+            <li>Without conscious thought, this group is highly competitve with how much they love their pets. Viewing other pet paintings will spark a bit of jealousy and cupidity</li>
+            <li>As sales increase, so to will the images on the site to attract more customers.</li>
+          </ul>
+        </td>
+    </tr>
+    <tr>
+        <td><h4>Past Customers</h4>
+        <p>Although this group is partly comprised of Wendy's inner circle, it also encompasses past work associates, acquaintances, and friends of friends. Mainly college educated with a vast array of internet capable gadgets and toys.</p></td>
+        <td>
+          <ul>
+            <li>Memory fades, even from happy, satisfied customers. They need a clear roadmap to return to buy again</li>
+            <li>Facebook has proven to be resilient through many controversies, but a number of people have sworn off. Past customers need another way of finding Wendy.</li> 
+          </ul>
+        </td>
+        <td>
+          <ul>
+            <li>Offer a newsletter signup to advertise latest offerings and showcases.</li>
+            <li>An ABOUT page on website to highlight Wendy's story would enhance recollection and customer loyalty.</li>
+          </ul>
+        </td>
+    </tr>
+</table>
+</section>
+ 
+<?php include 'includes/footer.php';?>
