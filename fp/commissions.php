716f8e1cc010b9563cafc208ba60d8d7e071d7ef
derekdesigns <82245525+derekdesigns@users.noreply.github.com>
2021-06-13 20:32:17 -0700

    Add files via upload

---
+++ fp/commissions.php
@@ -0,0 +1,16 @@
+<?php include 'includes/header.php';?>
+
+<h1>Commissions</h1>
+
+
+ </section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+  <h3></h3>
+
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php';?> 
---
+++ fp/index.php
@@ -0,0 +1,19 @@
+<?php include 'includes/header.php';?>
+
+<h1>Wendy Clark's Fine Art</h1>
+
+ <img src="images/FullSize_browndogredbandana.jpg" id="center" alt="browndog" />
+ <!-- <img src="images/FullSize_browndogredbandana.jpg" class="tablet" id="center" alt="browndog" /> -->
+ <!-- <img src="images/FullSize_browndogredbandana.jpg" class="phone" id="center" alt="browndog" /> -->
+ 
+ </section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+  <h3></h3>
+
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php';?> 
