3f43a3789760a2c23c8f12e77ada38d452b5a0f8
derekdesigns <82245525+derekdesigns@users.noreply.github.com>
2021-06-18 18:11:37 -0700

    Add files via upload

---
+++ flowchart.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php';?>
+       
+<style>
+    .embed-container { 
+        position: relative; 
+        padding-bottom: 56.25%; 
+        height: 0; 
+        overflow: hidden; 
+        max-width: 100%; 
+    } 
+    
+    .embed-container iframe, .embed-container object, .embed-container embed { 
+        position: absolute; 
+        top: 0; 
+        left: 0; 
+        width: 100%; 
+        height: 100%; 
+    }
+</style>
+        
+        <div class='embed-container'>
+            <iframe src="https://docs.google.com/document/d/1_jkoNID68kFYqI5KPnRbNKXeG5Hka0AYg0ZwIb-tKUQ/edit">
+            </iframe>
+        </div>
+ 
+<?php include 'includes/footer.php';?>
---
+++ template.php
@@ -0,0 +1,5 @@
+<?php include 'includes/header.php';?>
+       
+<p>Template</p>
+ 
+<?php include 'includes/footer.php';?>
