3d86f209f7a0e70de2e0e2229a8f85aab814f766
derekdesigns <82245525+derekdesigns@users.noreply.github.com>
2021-06-20 13:39:39 -0700

    Delete fp directory

--- fp/feedback.php
+++
@@ -1,5 +0,0 @@
-<div class="contact-feedback">
-    <h2>Your Comments Have Been Received!</h2>
-    <h3>Thanks for the input!</h3>
-    <h3>I'll respond via email within 48 hours, if you requested information.</h3>
-</div> 
